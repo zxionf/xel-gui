ea5567c9f8d62e41c1c3c80e9f9ea706e164c4a4
zxionf <zxionf@qq.com>
2026-05-25 23:00:21 +0800

    len wgpu

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,8 @@
 use std::sync::Arc;
 use winit::application::ApplicationHandler;
-use winit::event::WindowEvent;
-use winit::event_loop::{ControlFlow, EventLoop};
+use winit::event::{KeyEvent, WindowEvent};
+use winit::event_loop::{ActiveEventLoop, ControlFlow, EventLoop};
+use winit::keyboard::{KeyCode, PhysicalKey};
 use winit::window::{Window, WindowId};
 
 #[cfg(target_arch = "wasm32")]
@@ -20,7 +21,7 @@ impl State {
 
     pub fn resize(&mut self, _width: u32, _height: u32) {}
 
-    pub fn render(&mut self){
+    pub fn render(&mut self) {
         self.window.request_redraw();
     }
 }
@@ -32,47 +33,148 @@ struct App {
     state: Option<State>,
 }
 
-impl ApplicationHandler for App {
-    fn resumed(&mut self, event_loop: &winit::event_loop::ActiveEventLoop) {
-        self.window = Some(
-            event_loop
-                .create_window(Window::default_attributes())
-                .unwrap(),
-        )
+impl App {
+    pub fn new(#[cfg(target_arch = "wasm32")] event_loop: &EventLoop<State>) -> Self {
+        #[cfg(target_arch = "wasm32")]
+        let proxy = Some(event_loop.create_proxy());
+        Self {
+            state: None,
+            #[cfg(target_arch = "wasm32")]
+            proxy,
+        }
+    }
+
+    pub fn run() -> anyhow::Result<()> {
+        #[cfg(not(target_arch = "wasm32"))]
+        {
+            env_logger::init();
+        }
+        #[cfg(target_arch = "wasm32")]
+        {
+            console_log::init_with_level(log::Level::Info).unwrap_throw();
+        }
+
+        let event_loop = EventLoop::with_user_event().build()?;
+        #[cfg(not(target_arch = "wasm32"))]
+        {
+            let mut app = App::new();
+            event_loop.run_app(&mut app)?;
+        }
+        #[cfg(target_arch = "wasm32")]
+        {
+            let app = App::new(&event_loop);
+            event_loop.spawn_app(app);
+        }
+
+        Ok(())
+    }
+
+    #[cfg(target_arch = "wasm32")]
+    #[wasm_bindgen(start)]
+    pub fn run_web() -> Result<(), wasm_bindgen::JsValue> {
+        console_error_panic_hook::set_once();
+        run().unwrap_throw();
+        Ok(())
+    }
+}
+
+impl ApplicationHandler<State> for App {
+    fn resumed(&mut self, event_loop: &ActiveEventLoop) {
+        #[allow(unused_mut)]
+        let mut window_attributes = Window::default_attributes();
+
+        #[cfg(target_arch = "wasm32")]
+        {
+            use wasm_bindgen::JsCast;
+            use winit::platform::web::WindowAttributesExtWebSys;
+
+            const CANVAS_ID: &str = "canvas";
+
+            let window = wgpu::web_sys::window().unwrap_throw();
+            let document = window.document().unwrap_throw();
+            let canvas = document.get_element_by_id(CANVAS_ID).unwrap_throw();
+            let html_canvas_element = canvas.unchecked_into();
+            window_attributes = window_attributes.with_canvas(Some(html_canvas_element));
+        }
+
+        let window = Arc::new(event_loop.create_window(window_attributes).unwrap());
+
+        #[cfg(not(target_arch = "wasm32"))]
+        {
+            // If we are not on web we can use pollster to
+            // await the window creation
+            self.state = Some(pollster::block_on(State::new(window)).unwrap());
+        }
+
+        #[cfg(target_arch = "wasm32")]
+        {
+            // Run the future asynchronously and use the
+            // proxy to send the results to the event loop
+            if let Some(proxy) = self.proxy.take() {
+                wasm_bindgen_futures::spawn_local(async move {
+                    assert!(
+                        proxy
+                            .send_event(
+                                State::new(window)
+                                    .await
+                                    .expect("Unable to create canvas!!!")
+                            )
+                            .is_ok()
+                    )
+                });
+            }
+        }
+    }
+
+    #[allow(unused_mut)]
+    fn user_event(&mut self, _event_loop: &ActiveEventLoop, mut event: State) {
+        // This is where proxy.send_event() ends up
+        #[cfg(target_arch = "wasm32")]
+        {
+            event.window.request_redraw();
+            event.resize(
+                event.window.inner_size().width,
+                event.window.inner_size().height,
+            );
+        }
+        self.state = Some(event);
     }
 
     fn window_event(
         &mut self,
-        event_loop: &winit::event_loop::ActiveEventLoop,
-        window_id: WindowId,
-        event: winit::event::WindowEvent,
+        event_loop: &ActiveEventLoop,
+        _window_id: winit::window::WindowId,
+        event: WindowEvent,
     ) {
+        let state = match &mut self.state {
+            Some(canvas) => canvas,
+            None => return,
+        };
+
         match event {
-            WindowEvent::CloseRequested => {
-                println!("[event] 退出应用 id:{:?}", window_id);
-                event_loop.exit();
-            }
-            WindowEvent::ScaleFactorChanged {
-                scale_factor,
-                inner_size_writer,
-            } => {
-                println!(
-                    "[winit] 缩放因子 scale_factor:{} inner_size_writer:{:?}",
-                    scale_factor, inner_size_writer
-                )
-            }
+            WindowEvent::CloseRequested => event_loop.exit(),
+            WindowEvent::Resized(size) => state.resize(size.width, size.height),
             WindowEvent::RedrawRequested => {
-                self.window.as_ref().unwrap().request_redraw();
+                state.render();
             }
-            _ => (),
+            WindowEvent::KeyboardInput {
+                event:
+                    KeyEvent {
+                        physical_key: PhysicalKey::Code(code),
+                        state: key_state,
+                        ..
+                    },
+                ..
+            } => match (code, key_state.is_pressed()) {
+                (KeyCode::Escape, true) => event_loop.exit(),
+                _ => {}
+            },
+            _ => {}
         }
     }
 }
 
 fn main() {
     println!("[xel]xel >_<");
-    let event_loop = EventLoop::new().expect("[init] event loop 创建失败");
-    event_loop.set_control_flow(ControlFlow::Wait);
-    let mut app = App::default();
-    EventLoop::run_app(event_loop, &mut app).expect("[winit] event loop 运行失败");
+    App::run().expect("[xel] 启动失败");
 }
