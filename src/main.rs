9b1b7ae7f2c6a16a4bbb2498699949f27de5d2c2
zxionf <zxionf@qq.com>
2026-05-25 23:53:32 +0800

    待修复

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,120 @@
 use std::sync::Arc;
 use winit::application::ApplicationHandler;
+use winit::error::EventLoopError;
 use winit::event::{KeyEvent, WindowEvent};
-use winit::event_loop::{ActiveEventLoop, ControlFlow, EventLoop};
+use winit::event_loop::{ActiveEventLoop, EventLoop};
 use winit::keyboard::{KeyCode, PhysicalKey};
-use winit::window::{Window, WindowId};
-
-#[cfg(target_arch = "wasm32")]
-use wasm_bindgen::prelude::*;
-#[cfg(target_arch = "wasm32")]
-use winit::platform::web::EventLoopExtWebSys;
+use winit::window::Window;
 
 struct State {
+    surface: wgpu::Surface<'static>,
+    device: wgpu::Device,
+    queue: wgpu::Queue,
+    config: wgpu::SurfaceConfiguration,
+    is_surface_configured: bool,
     window: Arc<Window>,
 }
 
 impl State {
-    pub async fn new(window: Arc<Window>) -> anyhow::Result<Self> {
-        Ok(Self { window })
+    pub async fn new(window: Arc<Window>) -> Self {
+        let size = window.inner_size();
+        println!("[winit][窗口]大小:{}x{}", size.width, size.height);
+        let window = Arc::new(window);
+
+        let instance = wgpu::Instance::new(&wgpu::InstanceDescriptor {
+            backends: wgpu::Backends::VULKAN,
+            ..Default::default()
+        });
+
+        let surface = instance
+            .create_surface(Arc::clone(&window))
+            .expect("创建 Surface 失败");
+
+        let adapter = instance
+            .request_adapter(&wgpu::RequestAdapterOptions {
+                power_preference: wgpu::PowerPreference::default(),
+                compatible_surface: Some(&surface),
+                force_fallback_adapter: false,
+            })
+            .await
+            .expect("找不到合适的 GPU 适配器");
+
+        let adapter_info = adapter.get_info();
+        println!(
+            "[GPU] 名称: {}, 厂商: {}, 设备: {:?}, 后端: {:?}",
+            adapter_info.name, adapter_info.vendor, adapter_info.device_type, adapter_info.backend
+        );
+
+        let (device, queue) = adapter
+            .request_device(
+                &wgpu::DeviceDescriptor {
+                    label: None,
+                    required_features: wgpu::Features::empty(),
+                    required_limits: wgpu::Limits::default(),
+                },
+                None,
+            )
+            .await
+            .expect("创建 Device 失败");
+
+        let surface_caps = surface.get_capabilities(&adapter);
+        let surface_format = surface_caps
+            .formats
+            .iter()
+            .find(|f| f.is_srgb())
+            .copied()
+            .unwrap_or(surface_caps.formats[0]);
+
+        println!(
+            "[Surface] 选用格式: {:?}, 显示模式: {:?}",
+            surface_format, surface_caps.present_modes[0]
+        );
+
+        let config = wgpu::SurfaceConfiguration {
+            usage: wgpu::TextureUsages::RENDER_ATTACHMENT,
+            format: surface_format,
+            width: size.width,
+            height: size.height,
+            present_mode: wgpu::PresentMode::AutoVsync,
+            alpha_mode: surface_caps.alpha_modes[0],
+            view_formats: vec![],
+            desired_maximum_frame_latency: 2,
+        };
+        surface.configure(&device, &config);
+
+        Self {
+            surface,
+            device,
+            queue,
+            config,
+            window
+        }
     }
 
-    pub fn resize(&mut self, _width: u32, _height: u32) {}
+    pub fn resize(&mut self, width: u32, height: u32) {
+        if width > 0 && height > 0 {
+            self.config.width = width;
+            self.config.height = height;
+            self.surface.configure(&self.device, &self.config);
+            self.is_surface_configured = true;
+        }
+    }
 
-    pub fn render(&mut self) {
+    fn handle_key(&self, event_loop: &ActiveEventLoop, code: KeyCode, is_pressed: bool) {
+        match (code, is_pressed) {
+            (KeyCode::Escape, true) => event_loop.exit(),
+            _ => {}
+        }
+    }
+
+    fn update(mut self) {}
+
+    pub fn render(&mut self) -> anyhow::Result<()> {
         self.window.request_redraw();
+        // We can't render unless the surface is configured
+        if !self.is_surface_configured {
+            return Ok(());
+        }
     }
 }
 
@@ -44,37 +136,11 @@ impl App {
         }
     }
 
-    pub fn run() -> anyhow::Result<()> {
-        #[cfg(not(target_arch = "wasm32"))]
-        {
-            env_logger::init();
-        }
-        #[cfg(target_arch = "wasm32")]
-        {
-            console_log::init_with_level(log::Level::Info).unwrap_throw();
-        }
-
+    pub fn run() -> Result<(), EventLoopError> {
+        env_logger::init();
         let event_loop = EventLoop::with_user_event().build()?;
-        #[cfg(not(target_arch = "wasm32"))]
-        {
-            let mut app = App::new();
-            event_loop.run_app(&mut app)?;
-        }
-        #[cfg(target_arch = "wasm32")]
-        {
-            let app = App::new(&event_loop);
-            event_loop.spawn_app(app);
-        }
-
-        Ok(())
-    }
-
-    #[cfg(target_arch = "wasm32")]
-    #[wasm_bindgen(start)]
-    pub fn run_web() -> Result<(), wasm_bindgen::JsValue> {
-        console_error_panic_hook::set_once();
-        run().unwrap_throw();
-        Ok(())
+        let mut app = App::new();
+        event_loop.run_app(&mut app)
     }
 }
 
@@ -128,15 +194,6 @@ impl ApplicationHandler<State> for App {
 
     #[allow(unused_mut)]
     fn user_event(&mut self, _event_loop: &ActiveEventLoop, mut event: State) {
-        // This is where proxy.send_event() ends up
-        #[cfg(target_arch = "wasm32")]
-        {
-            event.window.request_redraw();
-            event.resize(
-                event.window.inner_size().width,
-                event.window.inner_size().height,
-            );
-        }
         self.state = Some(event);
     }
 
@@ -165,10 +222,7 @@ impl ApplicationHandler<State> for App {
                         ..
                     },
                 ..
-            } => match (code, key_state.is_pressed()) {
-                (KeyCode::Escape, true) => event_loop.exit(),
-                _ => {}
-            },
+            } => state.handle_key(event_loop, code, key_state.is_pressed()),
             _ => {}
         }
     }
